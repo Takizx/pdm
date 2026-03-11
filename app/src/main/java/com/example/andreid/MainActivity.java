8fd07a80ad802088bc5aa3360abf6ae53c2e5766
Schramm <lucas.schramm01@gmail.com>
2026-03-11 12:12:51 -0300

    contador e random

--- app/src/main/java/com/example/andreid/MainActivity.java
+++ app/src/main/java/com/example/andreid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.andreid;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.button);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            contador++;
+            Random random = new Random();
+            int r = random.nextInt(100);
+            tv.setText(Integer.toString(contador));
+        });
+
     }
 }
